69ffb6678e87bd614f07d3da6e262627c788aad6
Jihyeon Park <108461149+milhaud1201@users.noreply.github.com>
2024-03-05 15:09:33 +0900

    fix math

---
+++ conf.py
@@ -0,0 +1,16 @@
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
